bbb607fa30c3e5ad1234bbba980af8e5a16ac89d
Thomas Taulo <taulo@live.dk>
2025-08-18 21:42:35 +0200

    refactor: extract constants for star and shooting star counts

--- app/src/components/NightSky.jsx
+++ app/src/components/NightSky.jsx
@@ -2,21 +2,21 @@ import Star from "./Star.jsx";
 import {useEffect, useRef, useState} from "react";
 import ShootingStar from "./ShootingStar.jsx";
 
-const NightSky = () => {
-    const starsCount = 200;
-    const shootingStarsCount = 3;
+const STARS_COUNT = 200;
+const SHOOTING_STARS_COUNT = 3;
 
+const NightSky = () => {
     const starContainerRef = useRef(null);
     const [stars, setStars] = useState([]);
     const [shootingStars, setShootingStars] = useState([]);
 
     useEffect(() => {
         if (starContainerRef.current) {
-            setStars(Array.from({length: starsCount}).map((_, i) => (
+            setStars(Array.from({length: STARS_COUNT}).map((_, i) => (
                 <Star key={i} starContainerRef={starContainerRef.current}/>
             )));
 
-            setShootingStars(Array.from({length: shootingStarsCount}).map((_, i) =>
+            setShootingStars(Array.from({length: SHOOTING_STARS_COUNT}).map((_, i) =>
                 <ShootingStar key={i} starContainerRef={starContainerRef.current}/>
             ));
         }
