5866552715925c19ca2fb983d9f555d00beb3064
Thomas Taulo <taulo@live.dk>
2025-08-20 16:27:41 +0200

    feat: enhance NavBar with item selection and hover interactions, update styles for better consistency

--- app/src/components/NavBar/NavBar.jsx
+++ app/src/components/NavBar/NavBar.jsx
@@ -5,51 +5,74 @@ const NavBar = () => {
     {
       name: 'SKILL SET',
       isActive: false,
+      isSelected: false,
     },
     {
       name: 'EXPERIENCE',
       isActive: false,
+      isSelected: false,
     },
     {
       name: 'PROJECTS',
       isActive: false,
+      isSelected: false,
     },
     {
       name: 'ABOUT ME',
       isActive: false,
+      isSelected: false,
     },
     {
       name: 'CONTACT',
       isActive: false,
+      isSelected: false,
     },
   ]);
+
+  function selectNavItem(index) {
+    setNavItems((prev) =>
+      prev.map((item, idx) =>
+        idx === index
+          ? { ...item, isSelected: true }
+          : { ...item, isSelected: false }
+      )
+    );
+  }
+
+  function setNavItemActive(index) {
+    setNavItems((prev) =>
+      prev.map((item, idx) =>
+        idx === index
+          ? { ...item, isActive: true }
+          : { ...item, isActive: false }
+      )
+    );
+  }
+
+  function setNavItemInactive(index) {
+    setNavItems((prev) => prev.map((item) => ({ ...item, isActive: false })));
+  }
 
   return (
     <>
       <div className={'sm:my-15 fixed left-1/2 z-10 my-10 -translate-x-1/2'}>
         <div
           className={
-            'bg-menu backdrop-grayscale-25 flex items-center gap-5 text-ellipsis whitespace-nowrap rounded-xl border border-[rgba(62,71,137,0.3)] px-5 py-2 text-[.55rem] font-semibold text-white shadow sm:gap-10 sm:text-sm 2xl:text-sm'
+            'bg-menu backdrop-grayscale-25 flex items-center gap-5 text-ellipsis whitespace-nowrap rounded-2xl border border-[rgba(62,71,137,0.3)] px-5 py-3 text-[.55rem] font-semibold text-white shadow sm:gap-10 sm:text-sm 2xl:text-sm'
           }
         >
           {navItems.map((item, index) => (
             <div
               key={index}
               className={'relative flex flex-col'}
-              onClick={() =>
-                setNavItems((prev) =>
-                  prev.map((item, idx) =>
-                    idx === index
-                      ? { ...item, isActive: true }
-                      : { ...item, isActive: false }
-                  )
-                )
-              }
+              onClick={() => selectNavItem(index)}
+              onMouseEnter={() => setNavItemActive(index)}
+              onMouseLeave={() => setNavItemInactive(index)}
             >
               <div key={index} className={'cursor-pointer'}>
                 {item.name}
               </div>
-              {item.isActive && (
+              {(item.isActive || item.isSelected) && (
                 <div
                   className={
                     'animate-nav-item-active absolute -bottom-[3px] h-0.5 w-2/3 origin-center self-center bg-red-500'
