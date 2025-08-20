f4cc5922222d9017b677191fed656064aba2bd9d
Thomas Taulo <taulo@live.dk>
2025-08-20 16:28:29 +0200

    refactor: adjust Hero Button border-radius for style consistency

--- app/src/components/Hero/Button.jsx
+++ app/src/components/Hero/Button.jsx
@@ -3,7 +3,7 @@ const Button = () => {
     <>
       <div
         className={
-          'rounded-4xl grid place-items-center bg-white px-8 py-4 font-semibold text-black outline outline-white transition-colors duration-500 hover:cursor-pointer hover:bg-transparent hover:text-white'
+          'grid place-items-center rounded-2xl bg-white px-8 py-4 font-semibold text-black outline outline-white transition-colors duration-500 hover:cursor-pointer hover:bg-transparent hover:text-white'
         }
       >
         Explore My Work
