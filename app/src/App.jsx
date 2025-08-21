7209fdce0a88a4bba6c75ce2d97221f32b3c7e8b
Thomas Taulo <taulo@live.dk>
2025-08-21 15:37:01 +0200

    fix: correct file path typo in Experience component import

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,7 +2,7 @@ import NightSky from './components/NightSky/NightSky.jsx';
 import Hero from './components/Hero';
 import NavBar from './components/NavBar/NavBar.jsx';
 import TechStack from './components/Sections/TechStack/TechStack.jsx';
-import Experience from './components/Sections/Experience/Experience..jsx';
+import Experience from './components/Sections/Experience/Experience.jsx';
 import Layout from './Layout/Layout.jsx';
 
 function App() {
