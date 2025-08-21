8591e8aca201e475b5f63b13a07575e884a98b6d
Thomas Taulo <taulo@live.dk>
2025-08-21 12:51:11 +0200

    add margin bottom to Title.jsx

--- app/src/components/UI/Title/Title.jsx
+++ app/src/components/UI/Title/Title.jsx
@@ -1,10 +1,13 @@
 const Title = (props) => {
   return (
     <>
-      <h1 className={'text-center text-4xl font-bold text-white sm:text-start'}>
+      <h1
+        className={
+          'mb-5 text-center text-4xl font-bold text-white sm:text-start'
+        }
+      >
         {props.title}
       </h1>
-      ;
     </>
   );
 };
