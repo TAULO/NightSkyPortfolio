b1df9865d8939d6d5c37b0e11bd0e855065548fc
Thomas Taulo <taulo@live.dk>
2025-08-22 15:05:15 +0200

    fix: adjust NavBar positioning and improve shadow effect

--- app/src/components/NavBar/NavBar.jsx
+++ app/src/components/NavBar/NavBar.jsx
@@ -55,10 +55,10 @@ const NavBar = () => {
 
   return (
     <>
-      <div className={'sm:my-15 fixed left-1/2 z-10 my-10 -translate-x-1/2'}>
+      <div className={'fixed left-1/2 z-10 -translate-x-1/2 sm:top-15 bottom-10'}>
         <div
           className={
-            'bg-menu backdrop-grayscale-25 border-border flex items-center gap-5 text-ellipsis whitespace-nowrap rounded-2xl border px-5 py-3 text-[.55rem] font-semibold text-white shadow backdrop-blur-lg sm:gap-10 sm:text-sm 2xl:text-sm'
+            'bg-menu backdrop-grayscale-25 border-border flex items-center gap-5 text-ellipsis whitespace-nowrap rounded-2xl border px-5 py-3 text-[.55rem] font-semibold text-white shadow-lg backdrop-blur-lg sm:gap-10 sm:text-sm 2xl:text-sm'
           }
         >
           {navItems.map((item, index) => (
