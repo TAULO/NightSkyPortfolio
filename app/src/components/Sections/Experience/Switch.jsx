ecfa1055d6bfef748489d7413c125372aee71385
Thomas Taulo <taulo@live.dk>
2025-08-22 15:04:31 +0200

    refactor: update Switch component to accept props and simplify logic for reusability

--- app/src/components/Sections/Experience/Switch.jsx
+++ app/src/components/Sections/Experience/Switch.jsx
@@ -1,35 +1,20 @@
 import { useState, useRef, useEffect } from 'react';
 
-const Switch = () => {
-  const [items, setItems] = useState([
-    {
-      name: 'Work',
-      isSelected: true,
-    },
-    {
-      name: 'Education',
-      isSelected: false,
-    },
-    {
-      name: 'Personal',
-      isSelected: false,
-    },
-  ]);
-
+const Switch = ({ items, setItems }) => {
   const [backgroundStyle, setBackgroundStyle] = useState({
     width: 0,
-    transform: 'translateX(0px)'
+    transform: 'translateX(0px)',
   });
 
   const buttonRefs = useRef([]);
 
   useEffect(() => {
-    const selectedIndex = items.findIndex(item => item.isSelected);
+    const selectedIndex = items.findIndex((item) => item.isSelected);
     if (buttonRefs.current[selectedIndex]) {
       const selectedButton = buttonRefs.current[selectedIndex];
       setBackgroundStyle({
         width: selectedButton.offsetWidth,
-        transform: `translateX(${selectedButton.offsetLeft}px)`
+        transform: `translateX(${selectedButton.offsetLeft}px)`,
       });
     }
   }, []);
@@ -47,29 +32,33 @@ const Switch = () => {
     if (selectedButton) {
       setBackgroundStyle({
         width: selectedButton.offsetWidth,
-        transform: `translateX(${selectedButton.offsetLeft}px)`
+        transform: `translateX(${selectedButton.offsetLeft}px)`,
       });
     }
   }
 
   return (
     <>
-      <div className={'bg-secondary flex w-fit rounded-lg relative'}>
+      <div
+        className={
+          'bg-secondary relative flex w-fit self-center rounded-lg sm:self-start'
+        }
+      >
         <div
-          className="absolute top-0 bottom-0 bg-white rounded-lg transition-all duration-300 ease-in-out"
+          className="absolute bottom-0 top-0 rounded-lg bg-white transition-all duration-300 ease-in-out"
           style={{
             width: `${backgroundStyle.width}px`,
-            transform: backgroundStyle.transform
+            transform: backgroundStyle.transform,
           }}
         />
 
         {items.map((item, index) => (
           <div
             key={index}
-            ref={el => buttonRefs.current[index] = el}
-            className={`rounded-lg px-5 py-2 hover:cursor-pointer relative z-10 ${
+            ref={(el) => (buttonRefs.current[index] = el)}
+            className={`relative z-10 rounded-lg px-5 py-2 hover:cursor-pointer ${
               item.isSelected
-                ? 'text-black transition-colors duration-150 delay-100'
+                ? 'text-black transition-colors delay-100 duration-150'
                 : 'text-white transition-colors duration-100 hover:text-gray-200'
             }`}
             onClick={() => toggleItem(index)}
