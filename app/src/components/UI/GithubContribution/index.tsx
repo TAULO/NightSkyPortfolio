f25752a1d8b398181ebc6090cba02d33a5dee918
Thomas Taulo <taulo@live.dk>
2026-02-16 18:56:54 +0100

    chor: replace components with reuseable

--- app/src/components/UI/GithubContribution/index.tsx
+++ app/src/components/UI/GithubContribution/index.tsx
@@ -1,6 +1,8 @@
 import GitHubContributionCalendar from './GitHubContributionCalendar.tsx';
 import Title from '../Title/Title.tsx';
 import { useRef, useState } from 'react';
+import Button from '../Button/Button.tsx';
+import Input from '../Input/Input.tsx';
 
 const GithubContribution = () => {
   const [otherGithubUsername, setOtherGithubUsername] = useState<string>('');
@@ -9,18 +11,23 @@ const GithubContribution = () => {
   return (
     <div className={'flex flex-col'}>
       <Title title={'About Me'}></Title>
-      <div className={'flex flex-col gap-5'}>
-        <h3 className={'text-2xl font-bold text-white'}>
-          My GitHub Contributions
-        </h3>
-        <GitHubContributionCalendar
-          githubUsername={'TAULO'}
-        ></GitHubContributionCalendar>
-        <input type={'text'} className={'bg-red-800'} ref={inputRef}/>
-        <button onClick={() => setOtherGithubUsername(inputRef.current.value)} className={'bg-red-800'}>Click me</button>
-        <GitHubContributionCalendar
-          githubUsername={otherGithubUsername}
-        ></GitHubContributionCalendar>
+      <h3 className={'text-2xl font-bold text-white'}>
+        My GitHub Contributions
+      </h3>
+      <div className={'flex w-fit flex-col gap-8 mt-8'}>
+        <GitHubContributionCalendar githubUsername={'TAULO'} />
+        <GitHubContributionCalendar githubUsername={otherGithubUsername} />
+        <div className={'flex gap-2'}>
+          <Input
+            className={'grow'}
+            placeholder={'Type a GitHub username'}
+            ref={inputRef}
+          />
+          <Button
+            onClick={() => setOtherGithubUsername(inputRef.current.value)}
+            text={'Search'}
+          />
+        </div>
       </div>
     </div>
   );
