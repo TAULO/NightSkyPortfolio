db76ff2ad0d0014577fd70004630d91a6676a2de
Thomas Taulo <taulo@live.dk>
2026-02-17 11:23:30 +0100

    chor: remove github from home

--- app/src/components/Hero/Hero.tsx
+++ app/src/components/Hero/Hero.tsx
@@ -1,12 +1,7 @@
-import { githubSVG } from '../Sections/TechStack/TechSVG.tsx';
-
 const Hero = () => {
   return (
     <>
       <div className={'flex h-screen w-full justify-center'}>
-        <div className={'size-10 sm:top-15 absolute bottom-10 right-1/7 z-50 h-fit'}>
-          {githubSVG}
-        </div>
         <div className={'self-center flex flex-col gap-20'}>
         </div>
       </div>
