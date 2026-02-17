77c528243982d11748a109ee6f403d96f33bb8ec
Thomas Taulo <taulo@live.dk>
2026-02-17 15:13:06 +0100

    chor: mobile respinsiveness

--- app/src/components/Sections/Experience/ExperienceItem.tsx
+++ app/src/components/Sections/Experience/ExperienceItem.tsx
@@ -3,16 +3,16 @@ import { IExperienceItem } from './Experience.tsx';
 const experienceItem = (experiences: Array<IExperienceItem>) => {
   return experiences.map((item, index) => (
     <div
-      className={'flex flex-col gap-x-2 sm:flex-row sm:gap-x-10 sm:gap-y-5'}
+      className={'flex flex-col gap-x-2 md:flex-row md:gap-x-10 md:gap-y-5'}
       key={index}
     >
       <div
         className={
-          'flex flex-row items-center gap-3 sm:flex-col sm:items-center'
+          'flex flex-row items-center gap-3 md:flex-col md:items-center'
         }
       >
         <img
-          className={'h-fit w-14 rounded-2xl sm:w-20'}
+          className={'hidden h-fit w-14 rounded-2xl md:block md:w-20'}
           src={item.image?.src}
           alt={item.image?.alt}
         />
@@ -20,36 +20,44 @@ const experienceItem = (experiences: Array<IExperienceItem>) => {
         <div
           className={`${
             experiences.length > 1
-              ? 'hidden sm:block sm:h-full sm:w-[1px] sm:rounded-full sm:bg-white'
+              ? 'hidden md:block md:h-full md:w-[1px] md:rounded-full md:bg-white'
               : ''
           }`}
         ></div>
       </div>
 
-      <div className={'flex min-w-0 flex-col gap-4 text-white sm:gap-5'}>
+      <div className={'flex min-w-0 flex-col gap-4 text-white md:gap-5'}>
         <div>
-          <div
-            className={
-              'flex flex-col gap-1 sm:flex-row sm:items-baseline sm:gap-x-2'
-            }
-          >
-            <h1 className={'text-xl font-bold sm:text-2xl'}>{item.title}</h1>
-            <p className={'break-words font-extralight sm:whitespace-nowrap'}>
-              {item.place}
-            </p>
+          <div className={'flex justify-between'}>
+            <div
+              className={
+                'flex flex-col gap-1 md:flex-row md:items-baseline md:gap-x-2'
+              }
+            >
+              <h1 className={'text-xl font-bold md:text-2xl'}>{item.title}</h1>
+              <p className={'break-words font-extralight md:whitespace-nowrap'}>
+                {item.place}
+              </p>
+
+            </div>
+              <img
+                className={'h-fit w-14 rounded-2xl md:hidden'}
+                src={item.image?.src}
+                alt={item.image?.alt}
+              />
           </div>
 
           <div className={'mt-1 flex flex-col gap-0.5'}>
-            <p className={'text-sm sm:text-base'}>{item.role}</p>
-            <p className={'text-sm sm:text-base'}>{item.date}</p>
+            <p className={'text-sm md:text-base'}>{item.role}</p>
+            <p className={'text-sm md:text-base'}>{item.date}</p>
           </div>
         </div>
 
-        <div className={'flex flex-col gap-2 font-extralight sm:gap-0'}>
+        <div className={'flex flex-col gap-2 font-extralight md:gap-0'}>
           {item.tasks.map((task, index) => (
             <p
               className={
-                'relative pl-4 text-sm leading-relaxed before:absolute before:left-0 before:top-[0.6em] before:size-1 before:rounded-full before:bg-white before:shadow-[0_0_4px_1px_hsla(0,0%,100%,.3)] sm:text-base sm:leading-normal'
+                'relative pl-4 text-sm leading-relaxed before:absolute before:left-0 before:top-[0.6em] before:size-1 before:rounded-full before:bg-white before:shadow-[0_0_4px_1px_hsla(0,0%,100%,.3)] md:text-base md:leading-normal'
               }
               key={index}
             >
