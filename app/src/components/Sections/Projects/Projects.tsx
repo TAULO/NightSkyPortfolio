2275190feca9317fc72151de48b9f299773847fc
Thomas Taulo <taulo@live.dk>
2026-02-17 20:03:43 +0100

    feat: project component improve and added atheltica to this projects

--- app/src/components/Sections/Projects/Projects.tsx
+++ app/src/components/Sections/Projects/Projects.tsx
@@ -1,5 +1,87 @@
-const Projects = () => {
+import ProjectsItem from './ProjectsItem.tsx';
+import Title from '../../UI/Title/Title.tsx';
+import { JSX } from 'react';
+import {
+  csharpSVG,
+  dotnetcoreSVG,
+  nuxtSVG,
+  openAPISVG,
+  postgressqlSVG,
+  typescriptSVG,
+  vueSVG,
+} from '../TechStack/TechSVG.tsx';
 
+import testImage from '../../../assets/static/projects/athletica/foo.webp';
+
+export interface IProject {
+  name: string;
+  description: string;
+  hrefCode: string;
+  hrefLive?: string;
+  techStack: Array<{ svg: JSX.Element; name: string }>;
+  images: Array<{ src: string; alt: string }>;
 }
+
+const athleticaProject: IProject = {
+  name: 'Athletica',
+  description:
+    'Athletica is a mobile-first workout tracker designed to help you take control of your fitness journey. Easily log workouts, exercises, monitor progress, and visualize your improvements over time. Built with customization at its core, Athletica adapts to your goals — so you can train smarter and stay motivated.',
+  hrefCode: 'https://github.com/mahnac/Athletica',
+  techStack: [
+    {
+      svg: vueSVG,
+      name: 'VueJS',
+    },
+    {
+      svg: nuxtSVG,
+      name: 'Nuxt',
+    },
+    {
+      svg: typescriptSVG,
+      name: 'TypeScroåt',
+    },
+    {
+      svg: dotnetcoreSVG,
+      name: '.NET Aspire',
+    },
+    {
+      svg: csharpSVG,
+      name: 'C#',
+    },
+    {
+      svg: postgressqlSVG,
+      name: 'PostgreSQL',
+    },
+    {
+      svg: openAPISVG,
+      name: 'OpenAPI',
+    },
+  ],
+  images: [
+    {
+      src: testImage,
+      alt: 'Athletica',
+    },
+  ],
+};
+
+const projects: Array<IProject> = [athleticaProject];
+
+const Projects = () => {
+  return (
+    <div>
+      <Title title={'Projects'}></Title>
+      <div
+        className={
+          'grid w-full grid-cols-1 gap-4 lg:grid-cols-2 xl:grid-cols-4'
+        }
+      >
+        {projects.map((project, index) => (
+          <ProjectsItem {...project} key={index} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Projects;
--- app/src/components/Sections/Projects/ProjectsItem.tsx
+++ app/src/components/Sections/Projects/ProjectsItem.tsx
@@ -1,17 +1,71 @@
-import { githubSVG } from '../TechStack/TechSVG.tsx';
+import { IProject } from './Projects.tsx';
 
-const ProjectsItem = () => {
+const ProjectsItem = (project: IProject) => {
   return (
-    <div className={'w-64 h-84 border-2 border-secondary rounded-2xl text-white shadow'}>
-
-      <div className={'flex items-center w-full gap-2 text-[#9ba3e8]'}>
-        <div className={'size-8'}>
-          {githubSVG}
+    <div
+      id={'card'}
+      className={
+        'border-secondary bg-muted/20 h-full overflow-hidden rounded-3xl border shadow hover:cursor-pointer'
+      }
+    >
+      <img
+        src={project.images[0].src}
+        alt={project.images[0].alt}
+        className={
+          'object-top-left border-secondary h-40 w-full border-b object-cover lg:h-52'
+        }
+      />
+      <div
+        id={'card-body'}
+        className={'bg-secondary/40 flex flex-1 flex-col gap-2 px-4 py-2'}
+      >
+        <div className={'flex justify-between'}>
+          <h1 className={'mt-2 text-xl font-bold text-white'}>
+            {project.name}
+          </h1>
+          <div className={'self-end'}>
+            <a
+              href={project.hrefCode}
+              target="_blank"
+              rel="noopener noreferrer"
+              className={
+                'group/link flex items-center gap-1 text-sm text-blue-400 transition-colors hover:text-blue-400/80'
+              }
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className={
+                  'h-4 w-4 transform transition-transform group-hover/link:scale-110'
+                }
+              >
+                <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"></path>
+                <path d="M9 18c-4.51 2-5-2-7-2"></path>
+              </svg>
+              View Code
+            </a>
+          </div>
         </div>
-        <p>View Code</p>
+        <div className={'mb-1 flex flex-wrap gap-2'}>
+          {project.techStack.map((stack, index) => (
+            <div key={index} className={'size-5 shrink-0'}>
+              {stack.svg}
+            </div>
+          ))}
+        </div>
+        <p className={'mt-auto line-clamp-3 text-sm text-white/50'}>
+          {project.description}
+        </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default ProjectsItem;
