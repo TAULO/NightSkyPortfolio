7e778b04a9e64619f9a48c1925a1c70f07586219
Thomas Taulo <taulo@live.dk>
2026-02-19 16:42:52 +0100

    UI: navbar will always stay at top no matter screen size

--- app/src/components/NavBar/NavBar.tsx
+++ app/src/components/NavBar/NavBar.tsx
@@ -186,11 +186,7 @@ const NavBar = ({
 
   return (
     <>
-      <div
-        className={
-          'sm:top-15 fixed bottom-10 left-1/2 z-50 h-fit -translate-x-1/2'
-        }
-      >
+      <div className={'top-15 fixed left-1/2 z-50 h-fit -translate-x-1/2'}>
         <div
           className={
             'bg-menu backdrop-grayscale-25 border-border flex items-center gap-5 text-ellipsis whitespace-nowrap rounded-2xl border px-5 py-3 text-[.55rem] font-semibold text-white shadow-lg backdrop-blur-lg sm:gap-10 sm:text-sm 2xl:text-sm'
