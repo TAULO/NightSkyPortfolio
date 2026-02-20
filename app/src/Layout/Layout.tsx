8b1c6222ea2f8b2b51ab7a7c6b69c404f380fead
Thomas Taulo <taulo@live.dk>
2026-02-20 13:48:12 +0100

    UI: add animation to modal / change bg color / change card bg

--- app/src/Layout/Layout.tsx
+++ app/src/Layout/Layout.tsx
@@ -1,9 +1,14 @@
 import { ReactElement } from 'react';
+import { useModal } from '../components/UI/Modal/ModalProvider.tsx';
 
 const Layout = ({ children }: { children: Array<ReactElement> }) => {
+  const { isOpen } = useModal();
+
   return (
     <>
-      <div className={'bg-primary p-5 sm:p-20'}>
+      <div
+        className={`bg-primary p-5 transition-all duration-300 sm:p-20 ${isOpen ? 'brightness-25' : ''}`}
+      >
         <div className={'gap-35 flex flex-col'}>{children}</div>
       </div>
     </>
