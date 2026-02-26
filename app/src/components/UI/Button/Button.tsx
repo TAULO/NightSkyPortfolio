80998dd7c97867074068630c293dfd7bda089ce0
Thomas Taulo <taulo@live.dk>
2026-02-26 16:26:37 +0100

    UI: change button to fit theme

--- app/src/components/UI/Button/Button.tsx
+++ app/src/components/UI/Button/Button.tsx
@@ -6,7 +6,7 @@ const button = ({ text, ...props }: IButton) => {
   return (
     <button
       className={
-        'bg-secondary hover:bg-secondary/80 rounded px-4 py-2 font-semibold text-white transition-colors duration-300 hover:cursor-pointer'
+        'border-border hover:bg-secondary/25 bg-secondary rounded border px-4 py-2 font-semibold text-white transition-colors duration-300 hover:cursor-pointer'
       }
       {...props}
     >
