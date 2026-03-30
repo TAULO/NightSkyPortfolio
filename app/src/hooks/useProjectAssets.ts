2b5895a1aa50ad0f99fe2f1d1375936af25d3f55
Thomas Taulo <taulo@live.dk>
2026-03-30 20:59:05 +0200

    update asset paths to use /public directory and enable eager loading; renamed project asset files accordingly

--- app/src/hooks/useProjectAssets.ts
+++ app/src/hooks/useProjectAssets.ts
@@ -1,10 +1,10 @@
 export function useProjectAssets(projectFolderName: string) {
-  const modules = import.meta.glob('/src/assets/static/**/*');
+  const modules = import.meta.glob('/public/projects/**/*', { eager: true, as: 'url' });
 
   return Object.keys(modules)
     .filter((key) => key.includes(`/projects/${projectFolderName}/`))
     .map((key, index) => ({
-      src: key,
+      src: (modules[key] as string).replace('/public', ''),
       alt: `project-${projectFolderName}-${index}`,
     }));
 }
