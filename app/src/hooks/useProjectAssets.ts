ea2fd233bbcb63ec6c90c13b2d04c43a72eb2de0
Thomas Taulo <taulo@live.dk>
2026-04-13 20:31:00 +0200

    fix deploy

--- app/src/hooks/useProjectAssets.ts
+++ app/src/hooks/useProjectAssets.ts
@@ -1,10 +1,10 @@
 export function useProjectAssets(projectFolderName: string) {
-  const modules = import.meta.glob('/public/projects/**/*', { eager: true, as: 'url' });
+  const modules = import.meta.glob('/public/projects/**/*', { eager: true, query: '?url', import: 'default' });
 
   return Object.keys(modules)
     .filter((key) => key.includes(`/projects/${projectFolderName}/`))
     .map((key, index) => ({
-      src: (modules[key] as string).replace('/public', ''),
+      src: modules[key] as string,
       alt: `project-${projectFolderName}-${index}`,
     }));
 }
