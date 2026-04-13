d280db013f3aef74516784654b7ecf8a03304c82
Thomas Taulo <taulo@live.dk>
2026-04-13 20:49:27 +0200

    add return statement at /apo

--- app/api/github.ts
+++ app/api/github.ts
@@ -11,5 +11,5 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   });
 
   const data = await response.json();
-  res.status(200).json(data);
+  return res.status(200).json(data);
 }
